77be5f8e25cb0a9f0fb7fcd79183ff63e65d4e05
Dylan K. Taylor <dktapps@pmmp.io>
2025-02-17 17:51:39 +0000

    Update PHPStan

--- src/Server.php
+++ src/Server.php
@@ -699,7 +699,7 @@ class Server{
 
 	public function removeOp(string $name) : void{
 		$lowercaseName = strtolower($name);
-		foreach($this->operators->getAll() as $operatorName => $_){
+		foreach(Utils::promoteKeys($this->operators->getAll()) as $operatorName => $_){
 			$operatorName = (string) $operatorName;
 			if($lowercaseName === strtolower($operatorName)){
 				$this->operators->remove($operatorName);
